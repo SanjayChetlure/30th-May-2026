b3b9332625ac06c533a7c15de39cb061f412f730
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-06-19 21:33:55 +0530

    19th June 2026

--- Data Types/Ex2_Set.py
+++ Data Types/Ex2_Set.py
@@ -6,3 +6,54 @@ print(st)
 #get size of set object
 print(len(st))
 
+# print(st[0])   Invalid statement
+
+print("---Add New Single Element---")
+st.add("ganesh")
+print(st)
+
+
+print("---Add New multiple Elements---")
+st.update([75.2, "B+"])
+print(st)
+
+
+print("------Remove Element-----")
+st.remove("B+")    #Remove Specific Element -> Through error even if element does not exist
+print(st)
+
+st.discard(103)      # No error even if element does not exist
+print(st)
+
+st.pop()    #remove random element
+print(st)
+
+
+print("----copying set object-----")
+st1=st.copy()
+print(st1)
+
+
+print("----Sorting Set---")
+st2={40,50,10,40,30}   #{40,50,10,30}
+print("-----before sorting set object---")
+print(st2)
+lstObj=sorted(st2)    #[10, 20, 30, 40, 50]  sorted data stored into list object
+print(lstObj)
+
+
+print("------Check element present or not-----")
+# count & index fn are not present in set object
+print(10 in st2)
+
+
+print("----clear set object")
+st.clear()
+print(len(st))
+
+
+print("----delete set object")
+del st
+# print(st)       NameError: name 'st' is not defined
+
+
